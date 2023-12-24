76ec63e4570e41f3568c026c5d74ae0ba362cec1
akshat497 <122341089+akshat497@users.noreply.github.com>
2023-12-24 18:38:01 +0530

    ssl certificate deployed

--- restaurant_menu/src/admin/components/main/OrderManagement.js
+++ restaurant_menu/src/admin/components/main/OrderManagement.js
@@ -312,7 +312,7 @@ const OrderManagement = ({ ordersCopy, searchText, handleSearch }) => {
                 
                 <td >
                 <b>
-                <FaRupeeSign />  {row.data.Orders[0].couponDiscountPercentage!==0?(row.data.Orders[0].totalAmount/row.data.Orders[0].couponDiscountPercentage):row.data.Orders[0].couponDiscountPercentage}
+                <FaRupeeSign />  {row.data.Orders[0].couponDiscountPercentage!==0?row.data.Orders[0].totalAmount/row.data.Orders[0].couponDiscountPercentage:row.data.Orders[0].couponDiscountPercentage}
                 </b>
              
                 </td>
