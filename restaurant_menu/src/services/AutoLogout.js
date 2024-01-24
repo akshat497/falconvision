094ca24fc0c9cdf1cf1609a51821e210db5d6b70
akshat497 <122341089+akshat497@users.noreply.github.com>
2024-01-24 11:48:45 +0530

    autologut code changed

--- restaurant_menu/src/services/AutoLogout.js
+++ restaurant_menu/src/services/AutoLogout.js
@@ -2,33 +2,33 @@ import React, { useEffect } from "react";
 import { createAxiosInstance } from "./ApiInstance";
 
 const AutoLogout = () => {
-  useEffect(() => {
-    const checkTokenExpiration = async () => {
-      try {
-        await createAxiosInstance().get(
-          process.env.REACT_APP_BASE_URL + "check-expire"
-        );
-      } catch (error) {
-        if (error?.response?.data?.message === "jwt expired") {
-          // Token is expired, perform logout action
-          logout();
-        }
+  const logout = () => {
+    // Implement your logout logic here, e.g., redirect to login page
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("tokenExpirationTimer");
+    window.location.reload();
+  };
+
+  const checkTokenExpiration = async () => {
+    try {
+      await createAxiosInstance().get(
+        process.env.REACT_APP_BASE_URL + "check-expire"
+      );
+    } catch (error) {
+      if (error?.response?.data?.message === "jwt expired") {
+        // Token is expired, perform logout action
+        logout();
       }
-    };
+    }
+  };
 
-    const logout = () => {
-      // Implement your logout logic here, e.g., redirect to login page
-      sessionStorage.removeItem("token");
-      sessionStorage.removeItem("tokenExpirationTimer");
-      window.location.reload();
-    };
+  const startTokenExpirationTimer = () => {
+    // Set the expiration timer in both local and session storage
+    const expirationTimer = setTimeout(logout, 3600 * 1000);
+    sessionStorage.setItem("tokenExpirationTimer", expirationTimer.toString());
+  };
 
-    const startTokenExpirationTimer = () => {
-      // Set the expiration timer in both local and session storage
-      const expirationTimer = setTimeout(logout, 3600 * 1000);
-      sessionStorage.setItem("tokenExpirationTimer", expirationTimer.toString());
-    };
-
+  useEffect(() => {
     // Check if there is an existing expiration timer in session storage
     const existingTimer = sessionStorage.getItem("tokenExpirationTimer");
 
@@ -41,10 +41,12 @@ const AutoLogout = () => {
     startTokenExpirationTimer();
 
     // Set an interval to update the expiration timer in case of window closure
-    const updateExpirationTimerInterval = setInterval(
-      startTokenExpirationTimer,
-      60000 // Update every minute
-    );
+ // Set an interval to update the expiration timer in case of window closure
+const updateExpirationTimerInterval = setInterval(
+  startTokenExpirationTimer,
+  60000 // Update every minute
+);
+
 
     // Perform the initial check for token expiration
     checkTokenExpiration();
